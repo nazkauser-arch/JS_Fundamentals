4fd2af360c055e860eb70a276e3a57735f94294f
kauser naz <141734820+nazkauser-arch@users.noreply.github.com>
2026-07-22 22:25:24 +0500

    Implement checkout process with receipt generation
    Added functions to calculate total, discount, and final price, along with receipt generation for a shopping cart.

---
+++ day_03/refactor.js
@@ -0,0 +1,45 @@
+const cart = [
+    { name: "Laptop", price: 50000, quantity: 2 },
+    { name: "Mouse", price: 2000, quantity: 3 }
+]
+
+//Calculating Total
+function calcTotal(cart){
+    let total = 0
+    for (let item of cart){
+        total += item.price * item.quantity
+    }
+    return total
+}
+
+//calculating discount
+function calcDiscount(total){
+    let discount = 0.10
+    if (total > 10000){
+        return total * discount 
+    }
+    //return total
+}
+
+//calculating final price
+function calcFinalPrice(total, discount){
+    return total - discount
+}
+
+//generating receipt
+function generateReceipt(total, discount, finalPrice){
+    return `Total amount: ${total} \nDiscount applied: ${discount} \nFinal Amount to be paid: ${total}`
+}
+
+
+
+function processCheckout(cart){
+    let total = calcTotal(cart)
+    let discount = calcDiscount(total)
+    let finalPrice = calcFinalPrice(total, discount)
+    let receipt = generateReceipt(total, discount, finalPrice)
+
+    return receipt
+}
+
+console.log(processCheckout(cart))
