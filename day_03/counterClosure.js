e979541b92d32879d8f5791b77620ecf0bc2f726
kauser naz <141734820+nazkauser-arch@users.noreply.github.com>
2026-07-22 14:26:44 +0500

    Add closure-based counter implementation
    Implement a closure-based counter function that increments and returns the count each time it is called.

--- day_03/counterClosure.js
+++ day_03/counterClosure.js
@@ -1,1 +1,22 @@
+function createCounter(){
+    let count = 0
 
+    return function (){
+        count++
+        return count
+    }
+}
+
+const counter = createCounter()
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
