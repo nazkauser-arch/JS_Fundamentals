10288a701c8f98b270e45fd169516cadb12bdb93
Kauser <nazkauser413@gmail.com>
2026-07-23 20:19:49 +0500

    Implemented fetch error handling using try/catch

---
+++ day_04/fetchErrorHandling.js
@@ -0,0 +1,16 @@
+console.log("Start")
+
+async function getData(){
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json()
+
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getData()
+
+console.log("End")
