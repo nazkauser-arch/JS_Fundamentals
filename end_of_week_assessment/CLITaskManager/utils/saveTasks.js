f57a0fe9ed026c98674cae85e65c2b1de5f6175a
kauser naz <141734820+nazkauser-arch@users.noreply.github.com>
2026-07-28 02:50:54 +0500

    Fix saveTasks function to use tasks variable

--- end_of_week_assessment/CLITaskManager/utils/saveTasks.js
+++ end_of_week_assessment/CLITaskManager/utils/saveTasks.js
@@ -2,12 +2,11 @@ const fs = require("fs")
 
 function saveTasks() {
     try {
-        const data = JSON.stringify(TaskController, null, 2)
+        const data = JSON.stringify(tasks, null, 2)
         fs.writeFileSync("tasks.json", data)
     } catch (error) {
         console.log("Error saving tasks: ", error.message)
     }
-
 }
 console.log(data)
 
