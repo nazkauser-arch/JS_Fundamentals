80d6faac7aa847a9c50d47535cffa501b72fa0cc
kauser naz <141734820+nazkauser-arch@users.noreply.github.com>
2026-07-28 15:13:59 +0500

    Add console log for tasks data in saveTasks function
    Log the tasks data before saving to tasks.json.

--- end_of_week_assessment/CLITaskManager/utils/saveTasks.js
+++ end_of_week_assessment/CLITaskManager/utils/saveTasks.js
@@ -3,11 +3,12 @@ const fs = require("fs")
 function saveTasks() {
     try {
         const data = JSON.stringify(tasks, null, 2)
+        console.log(data)
+        
         fs.writeFileSync("tasks.json", data)
     } catch (error) {
         console.log("Error saving tasks: ", error.message)
     }
 }
-console.log(data)
 
 module.exports = saveTasks
