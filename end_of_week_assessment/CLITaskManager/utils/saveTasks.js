8538e5f46e15be42aed28c8ae6b3df2ad70418c8
kauser naz <141734820+nazkauser-arch@users.noreply.github.com>
2026-07-28 15:57:52 +0500

    Update saveTasks.js

--- end_of_week_assessment/CLITaskManager/utils/saveTasks.js
+++ end_of_week_assessment/CLITaskManager/utils/saveTasks.js
@@ -3,8 +3,7 @@ const fs = require("fs")
 function saveTasks(tasks) {
     try {
         const data = JSON.stringify(tasks, null, 2)
-        console.log(data)
-
+        
         fs.writeFileSync("tasks.json", data)
     } catch (error) {
         console.log("Error saving tasks: ", error.message)
