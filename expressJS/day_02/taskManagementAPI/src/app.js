610671063cede63a0b8db24b8bc4e7f7cd246406
Kauser <nazkauser413@gmail.com>
2026-08-02 16:12:55 +0500

    Implemented logic to log request's time and date

--- expressJS/day_02/taskManagementAPI/src/app.js
+++ expressJS/day_02/taskManagementAPI/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const taskRoutes = require("./routes/taskRoutes")
+const logger = require("./middleware/logger")
 const notFound = require("./middleware/notFound")
 const errorHandler = require("./middleware/errorHandler")
 
@@ -7,6 +8,8 @@ const errorHandler = require("./middleware/errorHandler")
 const app = express()
 
 app.use(express.json())
+
+app.use(logger)
 
 app.use("/api/tasks", taskRoutes)
 
