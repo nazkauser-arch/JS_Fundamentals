1e8bd1b3df0c305be2f56a824c752249c680714c
Kauser <nazkauser413@gmail.com>
2026-08-03 19:58:08 +0500

    Refactored

---
+++ mongoDB/day_01/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const taskRoutes = require("./routes/taskRoutes")
+const logger = require("./utils/logger")
+const notFound = require("./middleware/notFound")
+const errorHandler = require("./middleware/errorHandler")
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use(logger)
+
+app.use("/api/tasks", taskRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
+
+module.exports = app
