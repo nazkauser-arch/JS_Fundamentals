a8a555aab77406075d5afe0296fb36140b78df79
Kauser <nazkauser413@gmail.com>
2026-08-03 20:06:42 +0500

    Refactored

--- mongoDB/day_01/src/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-const notFound = (req, res, next) => {
-    const error = new Error(`Route not found: ${req.originalUrl}`)
-    error.statusCode = 404
-    next(error)
-}
-
-module.exports = notFound
