9c7b36c5b8dfe8afb4cb78d6b211fe05d3b8d980
Kauser <nazkauser413@gmail.com>
2026-08-25 18:58:52 +0500

    Removed unnecessary imports

--- devOps/task_manager/frontend/src/test/taskList.test.jsx
+++ devOps/task_manager/frontend/src/test/taskList.test.jsx
@@ -2,8 +2,6 @@ import { render, screen } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import { describe, test, expect } from "vitest"
 import TaskList from "../components/TaskList"
-import TaskCard from "../components/TaskCard"
-import userEvent from "@testing-library/user-event"
 
 describe("TaskList", () => {
   test("empty task list shows the correct message", () => {
